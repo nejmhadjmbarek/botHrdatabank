8b1759f7a996ad7b89c5ad8dc66f49a9ab6d1dd2
djo <djo@localhost>
2017-11-24 15:38:57 +0100

    commit add  @restController

--- src/main/java/com/example/controllers/TestController.java
+++ src/main/java/com/example/controllers/TestController.java
@@ -14,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RestController
 public class TestController {
 	@RequestMapping(value = "/test", method = RequestMethod.POST)
 	private @ResponseBody Map<String, Object> webhook(@RequestBody Map<String, Object> obj)
